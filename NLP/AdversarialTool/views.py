c9c193253199206b2b8312af8ceb2838f3cf70fc
Yvonne Zino <y_zino@coloradocollege.edu>
2021-12-01 13:58:18 -0700

    Basic machine learning example running

--- NLP/AdversarialTool/views.py
+++ NLP/AdversarialTool/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django import forms
+from AdversarialTool.ml_test import predict 
 
 class NewForm(forms.Form):
     inputText = forms.CharField(widget=forms.Textarea, label='')
@@ -9,10 +10,11 @@ def index(request):
     
     if request.method == "POST":
         # Take in the data the user submitted and save it as form
-        givenText = NewForm(request.POST)
+        givenText = request.POST
         textInputted=True
+        predictedText=predict()
         return render(request, "AdversarialTool/index.html", {
-        "form":NewForm(), "textInputted":textInputted, "givenText":givenText
+        "form":NewForm(), "textInputted":textInputted, "givenText":predictedText
     })
     else:
         return render(request, "AdversarialTool/index.html", {
