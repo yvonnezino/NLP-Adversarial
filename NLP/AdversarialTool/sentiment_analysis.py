8e94a5f8a854aa601cbcf1f7e87f828ca11aeaea
Yvonne Zino <y_zino@coloradocollege.edu>
2021-12-05 15:10:48 -0700

    Custom model trains, but with very poor results

--- NLP/AdversarialTool/sentiment_analysis.py
+++ NLP/AdversarialTool/sentiment_analysis.py
@@ -6,14 +6,14 @@ pos_text = "I enjoy studying computational algorithms."
 neg_text = "I dislike sleeping late everyday."
 
 
-result = sentiment_analysis(pos_text)[0]
-print("Label:", result['label'])
-print("Confidence Score:", result['score'])
-print()
+# result = sentiment_analysis(pos_text)[0]
+# print("Label:", result['label'])
+# print("Confidence Score:", result['score'])
+# print()
 
-result = sentiment_analysis(neg_text)[0]
-print("Label:", result['label'])
-print("Confidence Score:", result['score'])
+# result = sentiment_analysis(neg_text)[0]
+# print("Label:", result['label'])
+# print("Confidence Score:", result['score'])
 
 def predictSentiment(text):
     return sentiment_analysis(text)[0]
