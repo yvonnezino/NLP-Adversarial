a14f7d4e2e009e38b11812bc25e3783938fabb90
Stephen Sigman <stephen.sigman13@gmail.com>
2021-12-07 19:40:27 -0700

    Attacker yields dictionary of results

--- NLP/oa_demo2.py
+++ NLP/oa_demo2.py
@@ -16,16 +16,30 @@ def main():
     # BERT.SST is a pytorch model which is fine-tuned on SST-2. It uses Glove vectors for word representation.
     # The load operation returns a PytorchClassifier that can be further used for Attacker and AttackEval.
 
-    dataset = datasets.load_dataset("sst", split="train[:20]").map(function=dataset_mapping)
+    dataset = datasets.load_dataset("sst", split="train[:1]").map(function=dataset_mapping)
     # We load the sst-2 dataset using `datasets` package, and map the fields.
 
-    attacker = OpenAttack.attackers.PWWSAttacker()
+    attacker = OpenAttack.attackers.BERTAttacker()
     # After this step, we’ve initialized a PWWSAttacker and uses the default configuration during attack process.
 
     attack_eval = OpenAttack.AttackEval(attacker, victim)
     # Use the default implementation for AttackEval which supports seven basic metrics.
 
-    attack_eval.eval(dataset, visualize=True)
+    # eval_dict = attack_eval.eval(dataset, visualize=False)
+    # print(eval_dict)
+    simple_dataset = datasets.Dataset.from_dict({
+    "x": [
+        "I hate this movie.",
+        "I like this apple."
+    ],
+    "y": [
+        0, # 0 for negative
+        1, # 1 for positive
+    ]
+})
+    ieval = attack_eval.ieval(dataset)
+    for input in ieval:
+        print(input)
     # Using visualize=True in attack_eval.eval can make it displays a visualized result. This function is really useful for analyzing small datasets.
 
 if __name__ == "__main__":
