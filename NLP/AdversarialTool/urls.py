49b66a9282b3ca4dcb638c11132e3b94b201db63
Yvonne Zino <y_zino@coloradocollege.edu>
2021-12-13 11:46:13 -0700

    Adding informtional pages to the website

--- NLP/AdversarialTool/urls.py
+++ NLP/AdversarialTool/urls.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:699add673b074b13e7d4de624b3c7a8df599c33ba8a5af1470c49483a734672c
-size 187
+oid sha256:27a7165f6f025614d122ecfdcbbc8d50fa7f957bb51a1e8e2536c03398418bc0
+size 438
--- NLP/AdversarialTool/views.py
+++ NLP/AdversarialTool/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dcb9b66e105d671a0d1f830f6f0dd002565940a4cdba6975414a3a55f8721049
-size 3570
+oid sha256:a19872065e92c01b00850ff5f6379a89481921bc0e839b33fcaec8f44ea6d2c5
+size 3981
