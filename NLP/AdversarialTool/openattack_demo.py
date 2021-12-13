629aa8666fcd0e2b14448b476d03c0d732afdc39
Stephen Sigman <stephen.sigman13@gmail.com>
2021-12-12 17:04:39 -0700

    Add custom classifier

--- NLP/AdversarialTool/openattack_demo.py
+++ NLP/AdversarialTool/openattack_demo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df9b909939d39f1afb8f502f0ddd471e7584edfe7319a835f6c3863028de0e82
-size 2088
+oid sha256:a8f78a3de6c1353d0b354e10613db6f61f49a7a3994d60f534f3a8ccfd192239
+size 2818
